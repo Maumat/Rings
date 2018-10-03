cbe5922d6afd5da408d15a2f575eefeb57a178a8
Maumat <43821275+Maumat@users.noreply.github.com>
2018-10-03 11:00:39 -0500

    Add files via upload

---
+++ Sumamodulon.py
@@ -0,0 +1,13 @@
+#Suma modulo un entero n##
+
+def sumamod(x,y,n):
+	return (x+y)%n
+
+def prodmod(x,y,n):
+	return (x*y)%n
+
+
+#modulo=int(raw_input("Ingresa el modulo:"))
+#numero_1=int(raw_input("Ingresa el primer numero:"))
+#numero_2=int(raw_input("Ingresa el segundo numero:"))
+#print sumamod(numero_1,numero_2,modulo)
